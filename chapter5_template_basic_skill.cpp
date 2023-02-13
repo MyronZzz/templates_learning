1058b7be19ffabb6fa6f43636331b2b0d71c49f0
zhangzz <924204630@qq.com>
2023-02-13 21:37:10 +0800

    chapter5 part

--- chapter5_template_basic_skill.cpp
+++ chapter5_template_basic_skill.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 //关键字typename的使用
-//使用this->
+//使用 this-> 和 Base<T>
 //tips：
 ///////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
@@ -31,9 +31,56 @@ private:
 
 };
 
+//若基类非模板，很容易找到基类中的print1（）
+template<typename T>
+class Base{
+public:
+    void print1(){
+        cout << "this is Class Base" <<endl;
+    }
+};
+
+template<typename T>
+class Derived : public Base<T>{
+public:
+    void print1(){
+        cout << "Derived::print1() called"<<endl;
+    }
+    void print2(){
+        cout << "Base is " ;
+        print1();
+        cout << "Base is " ;
+        this->print1();
+        cout << "Base is ";
+        Base<T>::print1();
+        cout << "this is Class Derived" <<endl;
+    }
+};
+
+class Base2{
+public:
+    void print1(){
+        cout << "this is Class Base2" <<endl;
+    }
+};
+
+template<typename T>
+class Derived2 : public Base2{
+public:
+    void print2(){
+        cout << "Base is " ;
+        print1();
+        cout << "this is Class Derived" <<endl;
+    }
+};
+
 
 int main()
 {
     A<Value> a;
     a.GetValue1();
+    Derived<int> bbb;
+    bbb.print2();
+    Derived2<int> bbb2;
+    bbb2.print2();
 }
